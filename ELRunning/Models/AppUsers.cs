787d1a6500996a09702e873850511c1235677c17
Heinrich Ludike <heinrichl@maluti-x.com>
2020-10-28 10:02:50 +0200

    Identity and small fixes

--- ELRunning/Models/AppUsers.cs
+++ ELRunning/Models/AppUsers.cs
@@ -12,11 +12,12 @@ namespace ELRunning.Models
         public DateTime DateOfBirth { get; set; }
         public Guid CountryID { get; set; }
         public Guid GenderID { get; set; }
-        public Country Country { get; set; }
-        public Gender Gender { get; set; }
+        public virtual Country Country { get; set; }
+        public virtual Gender Gender { get; set; }
 
         public AppUser()
         {
+            //set default...
             CountryID = new Guid("BEAF367C-15D5-4C04-A801-871E28E33086");
             GenderID = new Guid("FF735D70-90D4-4029-82D5-924C0FC58FA0");
         }
