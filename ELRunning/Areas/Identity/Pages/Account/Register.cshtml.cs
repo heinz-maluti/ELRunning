9d8ec8f5ccf82775aec23045c17e207404fd4928
limitlessza <65598056+limitlessza@users.noreply.github.com>
2020-12-06 22:09:13 +0200

    Fixed login bug: After username changed the user could not login again. Added field and validation on the Registration screen for the client to enter a username.

--- ELRunning/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ ELRunning/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -47,6 +47,13 @@ namespace ELRunning.Areas.Identity.Pages.Account
         public class InputModel
         {
             [Required]
+            [DataType(DataType.Text)]
+            [Display(Name = "Username")]
+            [StringLength(30, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 3)]
+            public string UserName { get; set; }
+
+
+            [Required]
             [EmailAddress]
             [Display(Name = "Email")]
             public string Email { get; set; }
@@ -75,7 +82,7 @@ namespace ELRunning.Areas.Identity.Pages.Account
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
             {
-                var user = new AppUser { UserName = Input.Email, Email = Input.Email };
+                var user = new AppUser { UserName = Input.UserName, Email = Input.Email };
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
